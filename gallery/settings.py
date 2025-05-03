eca35d459c95fe50a1dd4705af2e58fbe0e8e55b
Alisher <alisher.arginbekov@mail.ru>
2025-05-03 23:56:30 +0500

    ready for Render deploy №2

--- gallery/settings.py
+++ gallery/settings.py
@@ -83,7 +83,6 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
